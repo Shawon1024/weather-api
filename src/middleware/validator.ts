af3568d44901db107b921b1712227b64d83c35dc
MD SAMSUDDUHA SHAWON <87534194+Shawon1024@users.noreply.github.com>
2024-02-29 20:50:07 +0000

    feat(src); added 'validator.ts' file

---
+++ src/middleware/validator.ts
@@ -0,0 +1,6 @@
+import { param } from "express-validator";
+
+export const validateCityName = param("city")
+    .isString()
+    .isIn(["london", "dublin"])
+    .withMessage("City Names Must be Either London or Dublin");
