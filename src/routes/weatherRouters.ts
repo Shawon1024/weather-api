754a454dd6bd26c0288ee2b627affbecb41d4cd8
MD SAMSUDDUHA SHAWON <87534194+Shawon1024@users.noreply.github.com>
2024-02-29 20:53:42 +0000

    feat(src); added 'weatherRouters.ts' file

---
+++ src/routes/weatherRouters.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import { getWeatherData } from "../controllers/weatherController.js";
+import { validateCityName } from "../middleware/validator.js";
+
+const router = express.Router();
+
+router.get("/:city", validateCityName, getWeatherData);
+
+export default router;
