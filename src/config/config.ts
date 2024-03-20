b1ac918e6b9dddf9dca82f776a08d70a00e56888
MD SAMSUDDUHA SHAWON <87534194+Shawon1024@users.noreply.github.com>
2024-03-20 02:01:42 +0000

    feat!; created 'helpers.ts' file in 'helpers' folder

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
-import algosdk from "algoskd";
+import algosdk from "algosdk";
 
 const algodToken = "a".repeat(64);
 const server: string ="http://localhoat";
@@ -7,7 +7,7 @@ const port: string = "4001";
 const mnemonic: string = "custom onion grab gain dragon divert hard swear habit matrix salmon decade olive normal proud keen eagle idea chicken enemy plastic carry swift about cream";
 
 export function getClient(): algosdk.Algodv2 {
-    let client = new algosdk.Algodv2(algodToken, server, posrt);
+    let client = new algosdk.Algodv2(algodToken, server, port);
     return client;
 }
 
---
+++ src/helpers/helpers.ts
@@ -0,0 +1,27 @@
+import algosdk from "algosdk";
+import { getClient, getAccount } from "../config/config.js";
+
+export const storeWeatherData = async (data: WeatherData): Promise<void> => {
+    try {
+        const client = getClient();
+        const account = getAccount();
+        const suggestedParams = await client.getTransactionParams().do();
+
+        const note = algosdk.encodeObj(data);
+
+        const txn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+            from: account.addr,
+            to: account.addr,
+            amount: 1000,
+            note: note,
+            suggestedParams: suggestedParams,
+        });
+
+        const signedTxn = txn.signTxn(account.sk);
+        const sendTxn = await client.sendRawTransaction(signedTxn).do();
+
+        console.log("Transaction ID: ", sendTxn.txnId);
+    } catch (error) {
+        console.error("Failed to Store Weather Data: ", error);
+    }
+};
