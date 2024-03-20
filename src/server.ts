5cca0c82e0e91d1e8718e4be541a140a2e318aca
MD SAMSUDDUHA SHAWON <87534194+Shawon1024@users.noreply.github.com>
2024-03-20 01:16:27 +0000

    feat!: installed 'cors'

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import weatherRoute from './routes/weatherRouters.js';
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/weather', weatherRoute);
 
