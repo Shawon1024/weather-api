ae516f534e3f009ec4ad8e5c67f65438d59cd9d9
MD SAMSUDDUHA SHAWON <87534194+Shawon1024@users.noreply.github.com>
2024-03-20 01:19:11 +0000

    feat!: created 'actions.ts' file in 'api' folder

--- dist/src/server.js
+++ dist/src/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import weatherRoute from './routes/weatherRouters.js';
+import cors from "cors";
 const app = express();
 const PORT = 3000;
+app.use(cors());
 app.use(express.json());
 app.use('/api/weather', weatherRoute);
 app.listen(PORT, () => {
