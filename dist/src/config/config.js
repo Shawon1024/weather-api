bd09dd2bddd3ea520b4562f465a1df110f9fa334
MD SAMSUDDUHA SHAWON <87534194+Shawon1024@users.noreply.github.com>
2024-03-20 02:05:37 +0000

    feat!: modified file

---
+++ dist/src/config/config.js
@@ -0,0 +1,14 @@
+import algosdk from "algosdk";
+const algodToken = "a".repeat(64);
+const server = "http://localhoat";
+const port = "4001";
+const mnemonic = "custom onion grab gain dragon divert hard swear habit matrix salmon decade olive normal proud keen eagle idea chicken enemy plastic carry swift about cream";
+export function getClient() {
+    let client = new algosdk.Algodv2(algodToken, server, port);
+    return client;
+}
+export function getAccount() {
+    let account = algosdk.mnemonicToSecretKey(mnemonic);
+    return account;
+}
+//# sourceMappingURL=config.js.map
--- dist/src/services/weatherService.js
+++ dist/src/services/weatherService.js
@@ -1,20 +1,25 @@
 import { faker } from '@faker-js/faker';
+import { storeWeatherData } from "../helpers/helpers.js";
 export const generateLondonWeatherData = () => {
     const generatedWeatherData = {
+        city: "London",
         temperature: faker.number.int({ min: -15, max: 30 }),
         humidity: faker.number.int({ min: 79, max: 86 }),
         wind: faker.number.int({ min: 2, max: 78 }),
         rain: faker.number.int({ min: 65, max: 75 }),
     };
+    storeWeatherData(generatedWeatherData).catch(console.error);
     return generatedWeatherData;
 };
 export const generateDublinWeatherData = () => {
     const generatedWeatherData = {
+        city: "Dublin",
         temperature: faker.number.int({ min: -15, max: 30 }),
         humidity: faker.number.int({ min: 79, max: 86 }),
         wind: faker.number.int({ min: 2, max: 78 }),
         rain: faker.number.int({ min: 65, max: 75 }),
     };
+    storeWeatherData(generatedWeatherData).catch(console.error);
     return generatedWeatherData;
 };
 //# sourceMappingURL=weatherService.js.map
